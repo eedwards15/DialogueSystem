1c511aff4ea6615a69ee73bc16525efc795fabff
Eddie <alexwerewolf2009@gmail.com>
2022-03-20 21:00:41 -0400

    Remove Node.

--- src/app/actions/dialogue.actions.ts
+++ src/app/actions/dialogue.actions.ts
@@ -2,6 +2,9 @@ import {createAction, props} from '@ngrx/store';
 import {Dialogue} from "../components/models/dialogue";
 
 export const NewDialogue = createAction('[Dialogue] New',props<{ payload: Dialogue }>() );
+export const RemoveDialogue = createAction('[Dialogue] Remove',props<{ payload: string }>() );
+
+
 
 // export const decrement = createAction('[Counter Component] Decrement');
 // export const reset = createAction('[Counter Component] Reset');
--- src/app/reducers/dialogue.reducers.ts
+++ src/app/reducers/dialogue.reducers.ts
@@ -1,6 +1,6 @@
 import { createReducer, on } from '@ngrx/store';
 import {Dialogue} from "../components/models/dialogue";
-import {NewDialogue} from "../actions/dialogue.actions";
+import {NewDialogue, RemoveDialogue} from "../actions/dialogue.actions";
 
 export interface State {
   dialogues: Dialogue[]
@@ -13,22 +13,37 @@ export const initialState: State = {
 const _dialogueReducer = createReducer(initialState,
   on(NewDialogue, (state,payload) =>   {
     return {...state,
-      dialogues:AppendDialogue(state, payload.payload)
+      dialogues:AppendDialogueAction(state, payload.payload)
+    };
+  }),
+  on(RemoveDialogue, (state,payload) =>   {
+    return {...state,
+      dialogues:RemoveDialogueAction(state, payload.payload)
     };
   })
-
 );
 
-function AppendDialogue(state:any, dialogue:Dialogue):Dialogue[]{
+function AppendDialogueAction(state:any, dialogue:Dialogue):Dialogue[]{
   let temp:Dialogue[] =  [];
   temp.push(dialogue)
   for (let i = 0; i < state.dialogues.length; i++) {
     temp.push(state.dialogues[i])
   }
-
   return temp;
-
 }
+
+function RemoveDialogueAction(state:any, uniqueId:string):Dialogue[]{
+  let temp:Dialogue[] =  [];
+  for (let i = 0; i < state.dialogues.length; i++)
+  {
+    if(state.dialogues[i].UniqueId != uniqueId) {
+      temp.push(state.dialogues[i])
+    }
+  }
+  return temp;
+}
+
+
 
 export function reducer(state:any, action:any) {
   return _dialogueReducer(state, action);
